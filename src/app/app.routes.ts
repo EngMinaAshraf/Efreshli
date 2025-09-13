b418495f6d1f70f5601b62d7b5b33a5a067bb6f8
mina_ <mina_@DESKTOP-1V9UF48>
2025-09-13 22:52:35 +0200

    hh

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,24 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './features/home/home.component';
-import { ProductListComponent } from './features/products/components/product-list/product-list';
-import { CategoryComponent } from './shared/components/category/category.component'; // Adjust path if needed
 import { CheckoutComponent } from './features/checkout/components/payment-methods/payment-methods';
-import { WishlistComponent } from './features/wishlist/wishlist.component';
 import { ContactUsComponent } from './features/contact-us/contact-us/contact-us';
+import { ProductListComponent } from './features/products/components/product-list/product-list';
+import { WishlistComponent } from './features/wishlist/wishlist.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'category/:category', component: CategoryComponent },
-  { path: 'checkout', component: CheckoutComponent }, // Ensure this matches the navigation
+// {
+//   path: 'category/:category',
+//   loadComponent: () =>
+//     import('./shared/components/category/category.component').then(m => m.CategoryComponent),
+//   data: { renderMode: 'client' } // 👈 force client rendering
+// },
+
+  { path: 'checkout', component: CheckoutComponent },
   { path: 'products', component: ProductListComponent },
   { path: 'wishlist', component: WishlistComponent },
-  {path: 'contact-us', component: ContactUsComponent},
+  { path: 'contact-us', component: ContactUsComponent },
+
+  // Fallback
   { path: '**', redirectTo: '' }
-  //   { path: 'products', component: ProductListComponent },
-  // { path: '', redirectTo: '/products', pathMatch: 'full' },
-  // { path: '**', redirectTo: '/products' } // إعادة توجيه أي مسار غير معروف إلى products
 ];
-// { path: '', redirectTo: 'products', pathMatch: 'full' },
-// { path: 'products', component: ProductListComponent }
--- src/main.server.ts
+++ src/main.server.ts
@@ -2,6 +2,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { config } from './app/app.config.server';
 
+
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
 export default bootstrap;
+
+// ✅ Correct import
+
+// ✅ Correct function
+// export function getPrerenderParams() {
+//   return [
+//     { route: '/category/fruits' },
+//     { route: '/category/vegetables' },
+//     { route: '/category/beverages' }
+//   ];
+// }
